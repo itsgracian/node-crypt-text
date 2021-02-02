a3c6b2f1e970b74a6a8e502d554907990b9b82cd
itsgracian <pgracian3@gmail.com>
2021-02-02 11:36:02 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const crypto = require('crypto');
+
+//this algorithm provide 24 bytes
+const algorithm = 'aes-192-cbc';
+
+const text = 'laravel';
+
+//create key
+const key = crypto.scryptSync('happy', 'salt', 24);
+
+//generate randomized initializtion vector
+// or use crypto.createRandomByte();
+const iv = Buffer.alloc(16, 0);
+
+
+const cipher = crypto.createCipheriv(algorithm, key, iv);
+
+//encryt data using streams
+
+let encrytedData = '';
+
+cipher.on('readable', ()=>{
+     let chunk;
+     while(null!==(chunk=cipher.read())){
+       encrytedData += chunk.toString('hex');
+     }
+});
+
+cipher.on('end', ()=>{
+  console.log(encrytedData);
+});
+
+cipher.write(text);
+cipher.end();
+
+
+// Decrypt data
+const decipher = crypto.createDecipheriv(algorithm, key, iv);
+
+let decryptedData = '';
+
+decipher.on('readable', ()=>{
+  let chunk;
+
+  while (null !== (chunk = decipher.read())) {
+    decryptedData += chunk.toString('utf8');
+  }
+
+});
+
+decipher.on('end', ()=>{
+  console.log(decryptedData);
+});
+
+decipher.write('4d0b637a10d2c8a3f58320864c1c55ec', 'hex');
+decipher.end();
